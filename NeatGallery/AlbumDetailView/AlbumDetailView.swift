c8a3aaca90b6097250212459e7f2fbda5b4e0f48
Farhad Bagherzadeh <farhad.bagherzadeh@rea-group.com>
2023-04-12 13:08:45 +1000

    added failedView

--- NeatGallery/AlbumDetailView/AlbumDetailView.swift
+++ NeatGallery/AlbumDetailView/AlbumDetailView.swift
@@ -11,18 +11,41 @@ struct AlbumDetailView: View {
   let album: AlbumModel?
 
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-      .navigationTitle(album?.name ?? "")
+    content
       .navigationViewStyle(.stack)
       .navigationBarTitleDisplayMode(.inline)
-      .toolbar {
-        ToolbarItem(placement: .navigationBarTrailing) {
-          Button {
-          } label: {
-            Image(systemName: "photo")
+
+  }
+}
+
+private extension AlbumDetailView {
+  @ViewBuilder var content: some View {
+    if let album {
+      Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        .navigationTitle(album.name)
+        .toolbar {
+          ToolbarItem(placement: .navigationBarTrailing) {
+            Button {
+            } label: {
+              Image(systemName: "photo")
+            }
           }
         }
-      }
+    } else {
+      failedView
+    }
+  }
+
+  var failedView: some View {
+    VStack {
+      Spacer()
+      Text("Something went wrong!")
+        .font(.title2)
+        .padding(.bottom, 4)
+      Text("Failed to load selected album")
+      Spacer()
+      Spacer()
+    }
   }
 }
 
@@ -30,8 +53,8 @@ struct AlbumDetailView_Previews: PreviewProvider {
   static var previews: some View {
     NavigationView {
       AlbumDetailView(
-        album: .init(name: "Adelaide", url: URL(string: "https://www.google.com")!)
-      )
+        album: .init(name: "Adelaide", url: URL(string: "https://www.google.com")!))
+
     }
   }
 }
