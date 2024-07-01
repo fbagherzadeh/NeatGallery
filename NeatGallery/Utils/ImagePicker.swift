21de15af7622d0661ad803b87f5b74f5c3f683cb
Farhad Bagherzadeh <farhad.bagherzadeh@rea-group.com>
2024-07-02 08:50:41 +1000

    fixed issue with no selection photo asking for deleting imported images

--- NeatGallery/Utils/ImagePicker.swift
+++ NeatGallery/Utils/ImagePicker.swift
@@ -9,36 +9,37 @@ import SwiftUI
 import PhotosUI
 
 struct ImagePicker: UIViewControllerRepresentable {
-  @Environment(\.presentationMode) private var presentationMode
-  let didFinishPickingMedia: ([PHPickerResult]) -> Void
+    @Environment(\.presentationMode) private var presentationMode
+    let didFinishPickingMedia: ([PHPickerResult]) -> Void
 
-  func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> PHPickerViewController {
-    let photoLibrary = PHPhotoLibrary.shared()
-    var config = PHPickerConfiguration(photoLibrary: photoLibrary)
-    config.selectionLimit = 0
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> PHPickerViewController {
+        let photoLibrary = PHPhotoLibrary.shared()
+        var config = PHPickerConfiguration(photoLibrary: photoLibrary)
+        config.selectionLimit = 0
 
-    let pickerViewController = PHPickerViewController(configuration: config)
-    pickerViewController.delegate = context.coordinator
-    return pickerViewController
-  }
-
-  func updateUIViewController(_ uiViewController: PHPickerViewController, context: UIViewControllerRepresentableContext<ImagePicker>) {}
-
-  func makeCoordinator() -> Coordinator {
-    Coordinator(self)
-  }
-
-  final class Coordinator: NSObject, PHPickerViewControllerDelegate {
-    var parent: ImagePicker
-    private var loadedImages: [UIImage] = []
-    
-    init(_ parent: ImagePicker) {
-      self.parent = parent
+        let pickerViewController = PHPickerViewController(configuration: config)
+        pickerViewController.delegate = context.coordinator
+        return pickerViewController
     }
 
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-      parent.didFinishPickingMedia(results)
-      parent.presentationMode.wrappedValue.dismiss()
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: UIViewControllerRepresentableContext<ImagePicker>) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
-  }
+
+    final class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            if !results.isEmpty {
+                parent.didFinishPickingMedia(results)
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
 }
